77e74f32f2e6d69a33a3d3348f95fa1cb7abaf0b
NFM <jwj0831@gmail.com>
2013-12-10 22:26:55 +0900

    update img

--- application/views/foods/upload_v.php
+++ application/views/foods/upload_v.php
@@ -26,7 +26,7 @@
 				</script>
 			</div>
 			<div class="form-group" id="food_name_input">
-				<input type="text" class="form-control" id="input_name" name="food_name" value="<?php echo set_value('food_name'); ?>" placeholder="Enter Food's name">
+				<input type="text" class="form-control" id="input_name" name="userfile" value="<?php echo set_value('userfile'); ?>" placeholder="Enter Food's name">
 			</div>
 			<div id="geoLoc-area">
 				<input type="hidden" class="geo-location" id="geo-result-input" name="geo-result" value="GEO_OK">
