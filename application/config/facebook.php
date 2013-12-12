990e0572cd7a263ffd4926d18b46be820cf61fb6
NFM <jwj0831@gmail.com>
2013-12-13 01:04:35 +0900

    config

---
+++ application/config/facebook.php
@@ -0,0 +1,5 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	$config['appId'] 			= '673638915991823';
+	$config['appSecret'] 		= '391d2893ca3a3b809a1eb33a63a39e85';
+?>
