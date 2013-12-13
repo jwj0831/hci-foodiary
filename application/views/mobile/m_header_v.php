430259db79e2f43f8884e1e9770d93ece05af19c
NFM <jwj0831@gmail.com>
2013-12-13 16:14:09 +0900

    update mobile header

--- application/views/mobile/m_header_v.php
+++ application/views/mobile/m_header_v.php
@@ -8,36 +8,41 @@
 		<meta name="author" content="">
 		<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
-		<title>Starter Template for Bootstrap</title>
-
+		<title>Welcome to Foodiary</title>
+	
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap-theme.min.css">
-
+		
+		<!-- Font Awesome's CDN -->
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		
 		<!-- Custom styles for this template -->
 		<link rel="stylesheet" type="text/css" href="/hci-foodiary/static/css/global.css" >
-		<link rel="stylesheet" type="text/css" href="/hci-foodiary/static/css/foodiary_m.css" >
-		<!--
-		<script>
-			$(document).ready(function(){
-				$('ahref="' + this.location.pathname + '"]'.parent().addClass('active');
-			})
-			
-		</script>
-		-->
-		<!-- Just for debugging purposes. Don't actually copy this line! -->
-		<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+		<link rel="stylesheet" type="text/css" href="/hci-foodiary/static/css/foodiary_w.css" >
+		
+
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 		<![endif]-->
+		
+		<!-- Front Page -->
 	</head>
 
 	<body>
+		<div id="wrapper">
+			<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container-top">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="/hci-foodiary">Foodiary Mobile</a>
+					</div>
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav pull">
 							<li>
