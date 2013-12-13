d589102fa069070e8ccc7a2844ed0d1c5ba87609
NFM <jwj0831@gmail.com>
2013-12-13 16:48:47 +0900

    up mobile header

--- application/views/mobile/m_header_v.php
+++ application/views/mobile/m_header_v.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="viewport" content="width=device-width,initial-scale=1, user-scalable=no" />
 		<meta name="description" content="">
 		<meta name="author" content="">
 		<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
