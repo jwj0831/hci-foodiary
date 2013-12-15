3a358e3adaedfdf2ea008f49dab0780817edbb0a
NFM <jwj0831@gmail.com>
2013-12-16 03:14:20 +0900

    up

--- application/views/mobile/m_footer_v.php
+++ application/views/mobile/m_footer_v.php
@@ -1,9 +1,2 @@
-		<!-- Bootstrap core JavaScript
-		================================================== -->
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
-		<script src="/hci-foodiary/static/scripts/jquery.starrr.js" type="text/javascript"></script>
 	</body>
 </html>
--- application/views/mobile/m_header_v.php
+++ application/views/mobile/m_header_v.php
@@ -11,22 +11,26 @@
 	
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+		
+		
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+		<script src="/hci-foodiary/static/scripts/jquery.starrr.js" type="text/javascript"></script>
 		<!-- Font Awesome's CDN -->
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 		
 		<!-- Custom styles for this template -->
 		<link rel="stylesheet" type="text/css" href="/hci-foodiary/static/css/global.css" >
 		<link rel="stylesheet" type="text/css" href="/hci-foodiary/static/css/foodiary_m.css" >
-		
-
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 		<![endif]-->
-		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+		
 		<!-- Front Page -->
 	</head>
 
