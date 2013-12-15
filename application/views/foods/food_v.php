daf195d08810788039141d0916411cb5dc2674a4
NFM <jwj0831@gmail.com>
2013-12-16 02:04:40 +0900

    update

--- application/views/foods/food_v.php
+++ application/views/foods/food_v.php
@@ -3,7 +3,7 @@
 		<div id="food_title">
 			<div>Your Food Records</div>
 		</div>
-		<div class="food_area">
+		<div id="food_body">
 <?php
 	$file_info = explode(".", $food->file_name);
 	if(is_file('./uploads/'.$file_info[0]."_thumb.".$file_info[1]))
@@ -16,21 +16,24 @@
 	}
 
 ?>
-			<div class="thumbnail" id="food_img_thumb">
+			<div class="thumbnail col-md-6" id="food_img_thumb">
                 <img class="img-rounded" id="food-record-img-thumb" src="<?php echo $thumb_img; ?>" alt="food_pic" />
 			</div>
-			<div class="form-group" id="user_name">
-				<?php echo $food->user_name; ?>
+			<div class="col-md-6" id="food_record_info">
+				<div class="form-group" id="user_name">
+					<span class="label label-default"><i class="fa fa-user"></i> User</span> <?php echo $food->user_name; ?>
+				</div>
+				<div class="form-group" id="reg_date">
+					<span class="label label-default"><i class="fa fa-calendar-o"></i> Date</span> <?php echo $food->reg_date; ?>
+				</div>
+				<div class="form-group" id="food_name">
+					<span class="label label-default"><img src="../../../static/images/food.png"></i> Food</span> <?php echo $food->food_name; ?>
+				</div>
+				<div class="form-group" id="food_comments">
+					<?php echo $food->comments; ?>
+				</div>
 			</div>
-			<div class="form-group" id="reg_date">
-				<?php echo $food->reg_date; ?>
-			</div>
-			<div class="form-group" id="food_name">
-				<?php echo $food->food_name; ?>
-			</div>
-			<div class="form-group" id="food_comments">
-				<?php echo $food->comments; ?>
-			</div>
+			
 
 			<script type="text/javascript">
   window.onload = function() {
