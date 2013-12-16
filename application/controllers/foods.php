b16d41aae02654cafed71624bd61cf5b8f1fb12f
NFM <jwj0831@gmail.com>
2013-12-16 12:23:52 +0900

    1

--- application/controllers/foods.php
+++ application/controllers/foods.php
@@ -23,7 +23,7 @@ class Foods extends CI_Controller {
 		parent::__construct();
 		$this->load->database();
 		$this->load->model('foodiary_m');
-		$this->load->helper(array('form', 'url', 'array'));
+		$this->load->helper(array('form', 'url', 'alert'));
 	}
 	
 	public function index()
